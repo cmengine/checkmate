7f1fb929535828151a479f774208db197eb4c85c
Matin Mohammadi <matinmn87@gmail.com>
2026-08-05 13:03:56 +0330

    chore: newname

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-//! Early development release.
-
-/// Placeholder.
-pub fn hello() {
-    println!("Checkmate, Lua!");
+/// Returns the embedded whitepaper.
+pub fn whitepaper() -> &'static str {
+    include_str!("../whitepaper.md")
 }

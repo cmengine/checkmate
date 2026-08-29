421019d9c382f393f92dd9c1b7dfd7a08a86286a
Matin Mohammadi <matinmn87@gmail.com>
2026-08-29 14:13:50 +0330

    chore: structure the project

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-/// Returns the embedded whitepaper.
-pub fn whitepaper() -> &'static str {
-    include_str!("../whitepaper.md")
-}
+#[cfg(feature = "compiler")]
+pub use cme_compiler as compiler;
+
+#[cfg(feature = "core")]
+pub use cme_core as core;
+
+#[cfg(feature = "interp")]
+pub use cme_interp as interp;
+
+#[cfg(feature = "runtime")]
+pub use cme_runtime as runtime;
